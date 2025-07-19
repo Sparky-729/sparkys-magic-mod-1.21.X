6883851110409a456fed4b70aa47f92a02e781c0
Sparky <parvorigami@gmail.com>
2025-07-19 23:08:54 +0530

    Initial commit

---
+++ src/main/java/sparkysmagicmod/modid/SparkysMagicMod.java
@@ -0,0 +1,17 @@
+package sparkysmagicmod.modid;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SparkysMagicMod implements ModInitializer {
+
+	public static final String MOD_ID = "sparkys-magic-mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/sparkysmagicmod/modid/SparkysMagicModClient.java
@@ -0,0 +1,12 @@
+package sparkysmagicmod.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SparkysMagicModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+
+}
